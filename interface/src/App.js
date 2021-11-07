2d1391af01673d00beadfc9428041a8c9b17750a
Em-kale <e.luhning@vitalpoint.ai>
2021-11-07 01:46:29 -0400

    added support for routing to multiple user interfaces + buttons to navigate

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useState, useEffect} from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 //page imports for routing
-import FrontPage from '/components/frontpage'
+import FrontPage from './components/frontpage'
 import StaffInterface from './components/staffInterface/staffinterface' 
 import CustomerInterface from './components/customerInterface/customerinterface';
 
@@ -20,23 +20,18 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <p>
         {data ? `${data}` : "loading..."}
         </p>
-      </header>
+      </header> */}
       <BrowserRouter>
-        <Switch>
-            <Route path="/">
-                <FrontPage />
-            </Route>
-            <Route path="/customer">
-                <CustomerInterface />
-            </Route>
-            <Route path="/staff">
-                <StaffInterface />
-            </Route>
-        </Switch>
+        <Routes>
+            <Route path="/" element={<FrontPage />} />
+            <Route path="/customer" element={<CustomerInterface/>} />
+            <Route path="/staff" element={<StaffInterface/>} />
+            <Route element={FrontPage} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
--- interface/src/components/frontpage.js
+++ interface/src/components/frontpage.js
@@ -3,19 +3,19 @@ import React from 'react'
 //material ui imports for consistent front end elements
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid'
-import Button from '@mui/material'
+import Button from '@mui/material/Button'
 
 function FrontPage(){
     return(
-        <Grid item>
+        <Grid container spacing={20}>
             <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
                 <Typography>Welcome to Emmitt's Book Shop</Typography>
             </Grid>
-            <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-                <Button href="/customer">Customer</Button>
+            <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
+                <Button variant="contained" href="/customer">Customer</Button>
             </Grid>
-            <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-                <Button href="/staff">Staff</Button>
+            <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
+                <Button variant="contained" href="/staff">Staff</Button>
             </Grid>
         </Grid>
     )
